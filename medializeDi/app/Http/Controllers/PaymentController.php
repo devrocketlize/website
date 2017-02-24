a7735a5f8e4ab3c847b38574c968fd0e07a027bf
unknown <paulo.hncandido@gmail.com>
2017-02-24 15:31:43 -0300

    teste leite com pera10

--- medializeDi/app/Http/Controllers/PaymentController.php
+++ medializeDi/app/Http/Controllers/PaymentController.php
@@ -43,66 +43,66 @@ class PaymentController extends Controller
     }
 
     // Cria Pagamento para o Pedido
-    $pagamento = new Pagamento();
-    $pagamento->pedido_id = $pedido->id;
-    $pagamento->provedor = $request->pagamento;
-    $pagamento->save();
+   // $pagamento = new Pagamento();
+   // $pagamento->pedido_id = $pedido->id;
+   // $pagamento->provedor = $request->pagamento;
+   // $pagamento->save();
 
-    switch ($pagamento->provedor) {
-      case 'paypal':
-        return redirect()->to('/paypal/pagamento/' . $pedido->id);
-        break;
-      case 'pagseguro':
-        return redirect()->to('/pagseguro/pagamento/' . $pedido->id);
-        break;
-      case 'mercadopago':
-        return redirect()->to('/mercadopago/pagamento/' . $pedido->id);
-        break;
-      case 'mercadopago':
-        return redirect()->to('/mercadopago/pagamento/' . $pedido->id);
-        break;
-    }
+    //switch ($pagamento->provedor) {
+      //case 'paypal':
+       // return redirect()->to('/paypal/pagamento/' . $pedido->id);
+       // break;
+     // case 'pagseguro':
+       // return redirect()->to('/pagseguro/pagamento/' . $pedido->id);
+        //break;
+     // case 'mercadopago':
+       // return redirect()->to('/mercadopago/pagamento/' . $pedido->id);
+       // break;
+     // case 'mercadopago':
+        //return redirect()->to('/mercadopago/pagamento/' . $pedido->id);
+       // break;
+    //}
   }
 
-  public function obrigado($pagamento)
-  {
-    $medias = Media::all();
+  //public function obrigado($pagamento)
+  //{
+   // $medias = Media::all();
 
-    if ($pagamento != null) {
+    //if ($pagamento != null) {
       // Informações do Pedido
-      $pedido = Pedido::find($pagamento);
+     // $pedido = Pedido::find($pagamento);
 
       // Informações do Serviço comprado
-      $servico = $pedido->servico;
+      //$servico = $pedido->servico;
 
       // Informações do Pagamento
-      $pagamento = $pedido->pagamento;
+     // $pagamento = $pedido->pagamento;
 
 
       //Dados do Email
-      $data =
-      [
-        'mServico' => $servico->quantidade . ' ' . $servico->tiposervico->desc,
-        'mNomeCliente' => $pedido->nomeCliente,
-        'mEmailCliente' => $pedido->emailCliente,
-        'mLink' => $pedido->link,
-        'mWhatsapp' => $pedido->whatsapp,
-        'mReferenciaPagamento' => $pagamento->referencia,
-        'mGatewayPagamento' => $pagamento->provedor,
-      ];
+     // $data =
+     // [
+       // 'mServico' => $servico->quantidade . ' ' . $servico->tiposervico->desc,
+       // 'mNomeCliente' => $pedido->nomeCliente,
+        //'mEmailCliente' => $pedido->emailCliente,
+       // 'mLink' => $pedido->link,
+        //'mWhatsapp' => $pedido->whatsapp,
+       // 'mReferenciaPagamento' => $pagamento->referencia,
+        //'mGatewayPagamento' => $pagamento->provedor,
+     // ];
 
-      $nomeCliente = $pedido->nomeCliente;
+     // $nomeCliente = $pedido->nomeCliente;
 
         
         //Email Teste para casos relacionados a caixa de Spam, envio demorado e alteracoes da aplicação.
 
-        Mail::send(['text' => 'mail'], $data, function($message) use ($nomeCliente) {
-          $message->to('paulo.hncandido@gmail.com', 'Serviço Marketing')->subject('Pedido relalizado por ' . $nomeCliente);
-          $message->from('newsletter@rocketlize.com');
-        });
+       // Mail::send(['text' => 'mail'], $data, function($message) use ($nomeCliente) {
+         // $message->to('paulo.hncandido@gmail.com', 'Serviço Marketing')->subject('Pedido relalizado por ' . $nomeCliente);
+         // $message->from('newsletter@rocketlize.com');
+        //});
   
-    }
+    //}
 
-    return view('compra-sucesso');
-  }
+   // return view('compra-sucesso');
+  //}
 }
