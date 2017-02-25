b93fdfd418468b15f0a3ba6f2d6711a3a2954b38
unknown <paulo.hncandido@gmail.com>
2017-02-25 14:17:47 -0300

    newsletter correcao23

--- medializeDi/resources/views/home.blade.php
+++ medializeDi/resources/views/home.blade.php
@@ -45,6 +45,20 @@
         <!-- END THEME STYLES -->
         <link rel="shortcut icon" href="{{ asset('favicon.ico')}}" /> </head>
 
+        <!--Start of Tawk.to Script-->
+            <script type="text/javascript">
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/58b1bbf293cfd35572fd9591/default';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+            </script>
+        <!--End of Tawk.to Script-->
+
     <body class="c-layout-header-fixed c-layout-header-mobile-fixed">
         <!-- BEGIN: LAYOUT/HEADERS/HEADER-1 -->
         <!-- BEGIN: HEADER -->
