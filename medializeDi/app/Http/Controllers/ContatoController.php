339a21de3459e6e72428de1823c27a3389c6fd9c
unknown <paulo.hncandido@gmail.com>
2017-02-25 19:01:28 -0300

    formulario de contato4

--- medializeDi/app/Http/Controllers/ContatoController.php
+++ medializeDi/app/Http/Controllers/ContatoController.php
@@ -11,9 +11,9 @@ class ContatoController extends Controller
 
     public function enviarMensagem(Request $request){
 
-    	$nomeCliente        	= $request->gett(trim(strip_tags('nome')));
-        $emailCliente        	= $request->gett(trim(strip_tags('email')));
-        $mensagemCliente    	= $request->gett(trim(strip_tags('mensagem')));
+    	$nomeCliente        	= $request->get(trim(strip_tags('nome')));
+        $emailCliente        	= $request->get(trim(strip_tags('email')));
+        $mensagemCliente    	= $request->get(trim(strip_tags('mensagem')));
 
         $data = ['nome' => $nomeCliente. ' '.$emailCliente,
                  'msgCliente' => $mensagemCliente,
