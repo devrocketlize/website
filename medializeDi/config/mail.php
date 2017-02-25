4657b2a1fad9570f6435fb3656bbdbf0aec482d9
unknown <paulo.hncandido@gmail.com>
2017-02-25 20:03:24 -0300

    formulario de contato9

--- medializeDi/config/mail.php
+++ medializeDi/config/mail.php
@@ -28,7 +28,8 @@ return [
     | the Mailgun mail service which will provide reliable deliveries.
     |
     */
- 'host' => env('MAIL_HOST', 'mail.autolikegram.com'),
+
+    'host' => env('MAIL_HOST', 'smtp.kinghost.net'),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 26),
+    'port' => env('MAIL_PORT', 587),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'contato@autolikegram.com',
-        'name' => 'SM Curtidas',
+        'address' => env('MAIL_FROM_ADDRESS', 'newsletter@rocketlize.com'),
+        'name' => env('MAIL_FROM_NAME', 'Newsletter da Rocketlize'),
     ],
 
     /*
@@ -85,27 +86,16 @@ return [
 
     'username' => env('MAIL_USERNAME'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | SMTP Server Password
-    |--------------------------------------------------------------------------
-    |
-    | Here you may set the password required by your SMTP server to send out
-    | messages from your application. This will be given to the server on
-    | connection so that the application will be able to send messages.
-    |
-    */
-
     'password' => env('MAIL_PASSWORD'),
 
     /*
     |--------------------------------------------------------------------------
-    | Sendmail System Path
+    | Markdown Mail Settings
     |--------------------------------------------------------------------------
     |
-    | When using the "sendmail" driver to send e-mails, we will need to know
-    | the path to where Sendmail lives on this server. A default path has
-    | been provided here, which will work well on most of your systems.
+    | If you are using Markdown based email rendering, you may configure your
+    | theme and component paths here, allowing you to customize the design
+    | of the emails. Or, you may simply stick with the Laravel defaults!
     |
     */
 
