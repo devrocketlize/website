e12eae6481bd264e52a761cbd4b4ce5f7d51b448
unknown <paulo.hncandido@gmail.com>
2017-02-26 17:58:59 -0300

    Area Administrativa29

--- medializeDi/resources/views/home.blade.php
+++ medializeDi/resources/views/home.blade.php
@@ -107,6 +107,7 @@
                               <th>Nome</th>
                               <th>E-mail</th>
                               <th>Whatsapp</th>
+                              <th>Whatsapp</th>
                             </tr>
                           </thead>
                           <tbody>
@@ -154,3 +155,6 @@
     </div>
 </section>
 @endsection
+
+
+<div class="bs-example" data-example-id="table-within-panel"> <div class="panel panel-default"> <div class="panel-heading">Panel heading</div> <div class="panel-body"> <p>Some default panel content here. Nulla vitae elit libero, a pharetra augue. Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit.</p> </div> <table class="table"> <thead> <tr> <th>#</th> <th>First Name</th> <th>Last Name</th> <th>Username</th> </tr> </thead> <tbody> <tr> <th scope="row">1</th> <td>Mark</td> <td>Otto</td> <td>@mdo</td> </tr> <tr> <th scope="row">2</th> <td>Jacob</td> <td>Thornton</td> <td>@fat</td> </tr> <tr> <th scope="row">3</th> <td>Larry</td> <td>the Bird</td> <td>@twitter</td> </tr> </tbody> </table> </div> </div>
