cc99907867d834ffd65929673a8174e45bedd4fe
unknown <paulo.hncandido@gmail.com>
2017-02-26 16:17:29 -0300

    Area Administrativa11

--- medializeDi/resources/views/home.blade.php
+++ medializeDi/resources/views/home.blade.php
@@ -51,10 +51,12 @@
 
                         <ul class="dropdown-menu" role="menu">
                             <li>
+                                <a href="{{ route('send') }}">Enviar campanha</a>
+                                
                                 <a href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
-                                    Logout
+                                    Sair
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -72,15 +74,36 @@
     <div class="container">
 
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+
+            <div class="col-md-12">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+                    <div class="panel-heading">Legenda</div>
+
+                    <div class="panel-body">
+
+                        <span class="label label-default">Pendente</span>
+                        <span class="label label-primary">Processando</span>
+                        <span class="label label-success">Aprovado</span>
+                        <span class="label label-info">Mediado</span>
+                        <span class="label label-warning">Rejeitado</span>
+                        <span class="label label-danger">Cancelado</span>
+                        <span class="label label-danger">Charge/Devolvido</span>
+
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Pedidos</div>
 
                     <div class="panel-body">
                         You are logged in!
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 </section>
