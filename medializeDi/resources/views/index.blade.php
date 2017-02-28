1bd21091c917d6b3ba3bea5e4cf7a9834631e819
paulohcandido <paulo.hncandido@gmail.com>
2017-02-28 14:52:35 -0300

    SEO23

--- medializeDi/resources/views/index.blade.php
+++ medializeDi/resources/views/index.blade.php
@@ -71,6 +71,15 @@
     </head>
 
     <body class="c-layout-header-fixed c-layout-header-mobile-fixed">
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.8&appId=137630659951001";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    </script>
         <!-- BEGIN: LAYOUT/HEADERS/HEADER-1 -->
         <!-- BEGIN: HEADER -->
         <header class="c-layout-header c-layout-header-4 c-layout-header-default-mobile" data-minimize-offset="80">
@@ -396,6 +405,10 @@
                                
                                <p class="propColor" itemprop="email" href="mailto:rocketlize@outlook.com">Atendimento no Skype: rocketlize@outlook.com </p>
 
+                            </div>
+
+                            <div>
+                                <div class="fb-share-button" data-href="https://rocketlize.com/" data-layout="button" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Frocketlize.com%2F&amp;src=sdkpreparse">Compartilhar</a></div>
                             </div>
 
                             <strong><p class="c-about"></p></strong>
