e01ea57c574e4c23413cf93bc63b1313bb65df6b
paulohcandido <paulo.hncandido@gmail.com>
2017-02-28 14:36:28 -0300

    SEO21

--- medializeDi/resources/views/index.blade.php
+++ medializeDi/resources/views/index.blade.php
@@ -12,8 +12,9 @@
         <meta http-equiv="Content-type" content="text/html; charset=utf-8">
         <meta content="Rocketlize, compre curtidas e seguidores para Instagram, curtidas para Facebook, inscritos para YouTube, seguidores para Twitter e SoundCloud." name="description" />
         <!-- BEGIN GLOBAL MANDATORY STYLES -->
-        <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700&amp;subset=all' rel='stylesheet' type='text/css'>
-
+        
+        <link href="assets/base/css/googlefonts.css" rel='stylesheet' type='text/css'>
+        
         <link href="{{ url('assets/plugins/socicon/socicon.css')}}" rel="stylesheet" type="text/css" />
         <link href="{{ url('assets/plugins/bootstrap-social/bootstrap-social.css')}}" rel="stylesheet" type="text/css" />
         <link href="{{ url('assets/plugins/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css" />
--- medializeDi/resources/views/layouts/master.blade.php
+++ medializeDi/resources/views/layouts/master.blade.php
@@ -12,7 +12,8 @@
         <meta http-equiv="Content-type" content="text/html; charset=utf-8">
         <meta content="Rocketlize, compre curtidas e seguidores para Instagram, curtidas para Facebook, inscritos para YouTube, seguidores para Twitter e SoundCloud." name="description" />
         <!-- BEGIN GLOBAL MANDATORY STYLES -->
-        <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700&amp;subset=all' rel='stylesheet' type='text/css'>
+        
+        <link href="assets/base/css/googlefonts.css" rel='stylesheet' type='text/css'>
 
         <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
 
