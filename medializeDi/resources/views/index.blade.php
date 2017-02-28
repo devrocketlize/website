f3060466c740f3dc33cbbd61ec2ad1b3dbd1440b
paulohcandido <paulo.hncandido@gmail.com>
2017-02-28 15:16:23 -0300

    SEO27

--- medializeDi/resources/views/index.blade.php
+++ medializeDi/resources/views/index.blade.php
@@ -404,8 +404,7 @@
                             <div itemscope itemtype="http://schema.org/Person">
                                
                                <p itemprop="email" href="mailto:rocketlize@outlook.com"><img crossorigin="propColor" src="https://rocketlize.com/assets/base/img/img-contato.png" title="Atendimento" alt="Atendimento"> </p>
-                               <p class="propColor" itemprop="email" href="mailto:rocketlize@outlook.com"> </p>
-
+                               
                             </div>
 
                             <div>
