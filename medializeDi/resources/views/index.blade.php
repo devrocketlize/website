d3a99506e9b5b0a2388e7498a36a720f4db4b104
paulohcandido <paulo.hncandido@gmail.com>
2017-03-29 15:29:46 -0300

    remocao ultimos scripts15

--- medializeDi/resources/views/index.blade.php
+++ medializeDi/resources/views/index.blade.php
@@ -153,37 +153,7 @@
                                 </div>
                             </li>
                             <!--END -->
-                            <!--BEGIN: SLIDE #2 -->
-                            <li data-transition="fade" data-slotamount="1" data-masterspeed="1000">
-                                <img alt="Facebook" title="Facebook" src="assets/base/img/content/backgrounds/bg_face.jpg" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat">
-                                <div class="tp-caption customin customout" data-x="center" data-y="center" data-hoffset="" data-voffset="-50" data-speed="500" data-start="1000" data-transform_idle="o:1;" data-transform_in="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:500;e:Back.easeInOut;"
-                                data-transform_out="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:600;e:Back.easeInOut;" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="600">
-                                    <h3 class="c-main-title-circle c-font-48 c-font-bold c-font-center c-font-uppercase c-font-white c-block"> SOMOS ESPECIALIZADOS
-                                        <br>EM GERAR FAMA EM SUAS MÍDIAS </h3>
-                                </div>
-                                <div class="tp-caption lft" data-x="center" data-y="center" data-voffset="110" data-speed="900" data-start="2000" data-transform_idle="o:1;" data-transform_in="x:100;y:100;rX:120;scaleX:0.75;scaleY:0.75;o:0;s:900;e:Back.easeInOut;"
-                                data-transform_out="x:100;y:100;rX:120;scaleX:0.75;scaleY:0.75;o:0;s:900;e:Back.easeInOut;">
-                                    <a href="https://rocketlize.com/comprar-curtidasfanpage-facebook" class="c-action-btn btn btn-lg c-btn-square c-theme-btn c-btn-bold c-btn-uppercase">CURTIDAS FAN PAGE</a>
-                                </div>
-                            </li>
-                            <!--END -->
-                            <!--BEGIN: SLIDE #3 -->
-                            <li data-transition="fade" data-slotamount="1" data-masterspeed="700" data-delay="6000" data-thumb="">
-                                <!-- THE MAIN IMAGE IN THE FIRST SLIDE -->
-                                <img alt="YouTube" title="YouTube" src="assets/base/img/content/backgrounds/bg-40.jpg" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="visible-xs" />
-                                <div class="rs-background-video-layer" data-forcerewind="on" data-volume="mute" data-videowidth="100%" data-videoheight="100%" data-videomp4="assets/base/media/video/video-2.mp4" data-videopreload="preload" data-videoloop="none"
-                                data-forceCover="1" data-aspectratio="16:9" data-autoplay="true" data-autoplayonlyfirsttime="false" data-nextslideatend="true"> </div>
-                                <div class="tp-caption customin customout" data-x="center" data-y="center" data-hoffset="" data-voffset="-30" data-speed="500" data-start="1000" data-transform_idle="o:1;" data-transform_in="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:500;e:Back.easeInOut;"
-                                data-transform_out="rX:0.5;scaleX:0.75;scaleY:0.75;o:0;s:600;e:Back.easeInOut;" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="600">
-                                    <h3 class="c-main-title-square c-font-55 c-font-bold c-font-center c-font-uppercase c-font-white c-block"> TORNANDO REALIDADE SEU SONHO,
-                                        <br>VENHA VIRALIZAR :-P </h3>
-                                </div>
-                                <div class="tp-caption lft" data-x="center" data-y="center" data-voffset="130" data-speed="900" data-start="2000" data-transform_idle="o:1;" data-transform_in="x:100;y:100;rX:120;scaleX:0.75;scaleY:0.75;o:0;s:900;e:Back.easeInOut;"
-                                data-transform_out="x:100;y:100;rX:120;scaleX:0.75;scaleY:0.75;o:0;s:900;e:Back.easeInOut;">
-                                    <a href="https://rocketlize.com/comprar-inscritos-youtube" class="c-action-btn btn btn-lg c-btn-square c-theme-btn c-btn-bold c-btn-uppercase">INSCRITOS NO YOUTUBE</a>
-                                </div>
-                            </li>
-                            <!--END -->
+                            
                         </ul>
                     </div>
                 </div>
