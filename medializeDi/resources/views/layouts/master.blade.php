2edacdbd2ddb6f3da33080153703e9fce520ef57
paulohcandido <paulo.hncandido@gmail.com>
2017-03-29 10:51:09 -0300

    melhorando carregamento

--- medializeDi/resources/views/layouts/master.blade.php
+++ medializeDi/resources/views/layouts/master.blade.php
@@ -11,41 +11,14 @@
         <meta content="width=device-width, initial-scale=1.0" name="viewport" />
         <meta http-equiv="Content-type" content="text/html; charset=utf-8">
         <meta content="Comprar curtidas e seguidores brasileiros reais no instagram. Comprar curtidas no facebook e inscritos no youtube, seguidores no twitter e soundcloud." name="description" />
-        <!-- BEGIN GLOBAL MANDATORY STYLES -->
-        
-        <link href="assets/base/css/googlefonts.css" rel='stylesheet' type='text/css'>
         <meta name="google-site-verification" content="RUagtzYkOYIJTQafihTYb7tAIXki2Ak8QAwgyWHtz90" />
 
+        <link href="assets/base/css/googlefonts.css" rel='stylesheet' type='text/css'>
         <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
-
-
-        <link href="{{ url('assets/plugins/socicon/socicon.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{ url('assets/plugins/bootstrap-social/bootstrap-social.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{ url('assets/plugins/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{ url('assets/plugins/simple-line-icons/simple-line-icons.min.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{ url('assets/plugins/animate/animate.min.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{ url('assets/plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
-        <!-- END GLOBAL MANDATORY STYLES -->
-        
-        <!-- BEGIN: BASE PLUGINS  -->
-        <link href="{{ url('assets/plugins/revo-slider/css/settings.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{ url('assets/plugins/revo-slider/css/layers.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{ url('assets/plugins/revo-slider/css/navigation.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{ url('assets/plugins/cubeportfolio/css/cubeportfolio.min.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{ url('assets/plugins/owl-carousel/assets/owl.carousel.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{ url('assets/plugins/fancybox/jquery.fancybox.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{ url('assets/plugins/slider-for-bootstrap/css/slider.css')}}" rel="stylesheet" type="text/css" />
-        <!-- END: BASE PLUGINS -->
-        
-        <!-- BEGIN: PAGE STYLES -->
         <link href="{{ url('assets/plugins/ilightbox/css/ilightbox.css')}}" rel="stylesheet" type="text/css" />
-        <!-- END: PAGE STYLES -->
-        <!-- BEGIN THEME STYLES -->
-        <link href="{{ url('assets/base/css/plugins.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{ url('assets/base/css/components.css')}}" id="style_components" rel="stylesheet" type="text/css" />
         <link href="{{ url('assets/base/css/themes/default.css')}}" rel="stylesheet" id="style_theme" type="text/css" />
-        <link href="{{ url('assets/base/css/custom.css')}}" rel="stylesheet" type="text/css" />
-        <!-- END THEME STYLES -->
+      
+        
         <link rel="shortcut icon" href="{{ url('assets/favicon.ico')}}" />
 
         <!--Start of Tawk.to Script-->
