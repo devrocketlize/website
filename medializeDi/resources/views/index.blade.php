aef97c19ead0681e99a3c0fa06866171222720ee
paulohcandido <paulo.hncandido@gmail.com>
2017-03-29 16:00:08 -0300

    remocao ultimos scripts18

--- medializeDi/resources/views/index.blade.php
+++ medializeDi/resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <!-- BEGIN HEAD -->
     <head>
         <meta charset="utf-8" />
-        <title>Rocketlize - Comprar seguidores reais, curtidas e views no Instagram</title>
+        <title>Rocketlize - Comprar seguidores e curtidas no Instagram</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1.0" name="viewport" />
         <meta http-equiv="Content-type" content="text/html; charset=utf-8">
