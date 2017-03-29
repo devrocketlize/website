88ef11950052dcab17b42ca5a84f88185c4f9cdf
paulohcandido <paulo.hncandido@gmail.com>
2017-03-29 15:40:59 -0300

    remocao ultimos scripts17

--- medializeDi/resources/views/index.blade.php
+++ medializeDi/resources/views/index.blade.php
@@ -421,9 +421,6 @@
         <!-- BEGIN: LAYOUT PLUGINS -->
         <script src="{{ url('assets/plugins/revo-slider/js/jquery.themepunch.tools.min.js')}}" type="text/javascript"></script>
         <script src="{{ url('assets/plugins/revo-slider/js/jquery.themepunch.revolution.min.js')}}" type="text/javascript"></script>
-        <script src="{{ url('assets/plugins/revo-slider/js/extensions/revolution.extension.slideanims.min.js')}}" type="text/javascript"></script>
-        <script src="{{ url('assets/plugins/revo-slider/js/extensions/revolution.extension.layeranimation.min.js')}}" type="text/javascript"></script>
-        
         
         <script src="{{ url('assets/plugins/cubeportfolio/js/jquery.cubeportfolio.min.js')}}" type="text/javascript"></script>
         <script src="{{ url('assets/plugins/owl-carousel/owl.carousel.min.js')}}" type="text/javascript"></script>
